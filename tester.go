6e1e0cc16ff00264910641cb7f7cbee924914d8f
CledsonSilvaAraujo <CledsonSilvaAraujo>
2024-02-24 18:14:38 -0300

    feat: basic auth

---
+++ tester.go
@@ -0,0 +1,35 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"net/http"
+// )
+
+// func main() {
+// 	url := "localhost:8080/"
+// 	method := "GET"
+
+// 	client := &http.Client{}
+// 	req, err := http.NewRequest(method, url, nil)
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	req.Header.Add("Authorization", "Basic dXNlcm5hbWU6cGFzc3dvcmQ=")
+
+// 	res, err := client.Do(req)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	defer res.Body.Close()
+
+// 	body, err := ioutil.ReadAll(res.Body)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	fmt.Println(string(body))
+// }
