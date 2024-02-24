1191c184abf76359a96e45091054881345bfe4c8
CledsonSilvaAraujo <CledsonSilvaAraujo>
2024-02-24 17:23:35 -0300

    remove dead code

--- main.go
+++ main.go
@@ -11,23 +11,7 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
-func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":     "1",
-		"alunos": "Cledson Silva",
-	})
-}
-
-// func ExibeVideos(c *gin.Context) {
-// 	c.JSON(200, videoController.FindAll())
-// }
-
-func SalvaVideos(c *gin.Context) {
-	c.JSON(200, videoController.Save(c))
-}
-
 func main() {
-	// r := gin.Default()
 	server := gin.New()
 	server.Use(gin.Recovery(), gin.Logger())
 
@@ -36,18 +20,14 @@ func main() {
 			"message": "Hello, Gin!",
 		})
 	})
-	// server.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "Pong, Gin!",
-	// 	})
-	// })
 
 	server.GET("/videos", func(c *gin.Context) {
 		c.JSON(200, videoController.FindAll())
 	})
+
 	server.POST("/videos", func(c *gin.Context) {
 		c.JSON(200, videoController.Save(c))
 	})
 
-	server.Run(":8080") // listen and serve on 0.0.0.0:8080
+	server.Run(":8080")
 }
